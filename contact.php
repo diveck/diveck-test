7b65faffc6d2872874d17a00239aaa39cc4f42b1
diveck <44412049+diveck@users.noreply.github.com>
2018-10-23 22:05:34 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,131 @@
+<?php
+if(isset($_POST['email'])) {
+ 
+    // Edita las líneas siguientes con tu dirección de correo y asunto
+ 
+    $email_to = "dj-diveck@hotmail.com";
+ 
+    $email_subject = "Clientes Instagram";   
+ 
+    function died($error) {
+ 
+        // si hay algún error, el formulario puede desplegar su mensaje de aviso
+ 
+        echo "Lo sentimos, hay un error en sus datos y el formulario no puede ser enviado. ";
+ 
+        echo "Detalle de los errores.<br /><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "Porfavor corrije los errores e inténtelo de nuevo.<br /><br />";
+        die();
+    }
+ 
+    // Se valida que los campos del formulairo estén llenos
+ 
+    if(!isset($_POST['first_name']) ||
+
+        !isset($_POST['last_name']) ||
+ 
+        !isset($_POST['email']) ||
+ 
+        !isset($_POST['telephone']) ||
+ 
+        !isset($_POST['message'])) {
+ 
+        died('Lo sentimos pero parece haber un problema con los datos enviados.');       
+ 
+    }
+ //Valor "name" nos sirve para crear las variables que recolectaran la información de cada campo
+ 
+    $first_name = $_POST['first_name']; // requerido
+
+    $last_name = $_POST['first_name']; // requerido
+ 
+    $email_from = $_POST['email']; // requerido
+ 
+    $telephone = $_POST['telephone']; // no requerido 
+ 
+    $message = $_POST['message']; // requerido
+ 
+    $error_message = "Error";
+ 
+//Verificar que la dirección de correo sea válida 
+ 
+   $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= 'La dirección de correo proporcionada no es válida.<br />';
+ 
+  }
+ 
+//Validadacion de cadenas de texto
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$first_name)) {
+ 
+    $error_message .= 'El formato del nombre no es válido<br />';
+ 
+  }
+ 
+ 
+  if(strlen($message) < 2) {
+ 
+    $error_message .= 'El formato del texto no es válido.<br />';
+ 
+  }
+ 
+  if(strlen($error_message) < 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+//Plantilla de mensaje
+ 
+    $email_message = "Contenido del Mensaje.\n\n";
+ 
+ 
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+ 
+ 
+    $email_message .= "Nombre: ".clean_string($first_name)."\n";
+
+    $email_message .= "Nombre: ".clean_string($last_name)."\n";
+ 
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+ 
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+ 
+    $email_message .= "Mensaje: ".clean_string($message)."\n";
+ 
+ 
+//Encabezados
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);  
+ 
+?>
+ 
+<!-- Mensaje de Éxito-->
+ 
+<meta http-equiv='refresh' content='1;url=http://socialmarketingcolo.000webhostapp.com/thank-you.html'>
+ 
+<?php 
+}
+?>
